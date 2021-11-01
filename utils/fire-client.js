99a46e25bb27097c1d62fa5561d80edbd34cbcd9
Sukanta Saha <sukantasaha@Sukantas-MacBook-Air.local>
2021-11-02 03:21:06 +0530

    sukanta: firebase config fix

--- utils/fire-client.js
+++ utils/fire-client.js
@@ -1,22 +1,22 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import { getAuth } from "firebase/auth"
-import { getStorage } from "firebase/storage"
-import "firebase/auth"
-import "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import "firebase/auth";
+import "firebase/firestore";
 
 const clientCredentials = {
-  apiKey: "AIzaSyDv5E8bODKBG3zRremY2mI_zrMPCUXQeck",
-  authDomain: "next-977ce.firebaseapp.com",
-  projectId: "next-977ce",
-  storageBucket: "next-977ce.appspot.com",
-  messagingSenderId: "1098288107226",
-  appId: "1:1098288107226:web:3734ad0f0d5ec214209fe3",
-}
+  apiKey: "AIzaSyCj5wTzEtLMxQaFv80az73SN1ZggtIWNZg",
+  authDomain: "textweb-c14d1.firebaseapp.com",
+  projectId: "textweb-c14d1",
+  storageBucket: "textweb-c14d1.appspot.com",
+  messagingSenderId: "378558592654",
+  appId: "1:378558592654:web:ae5f366d40f80a79383bdf",
+};
 
-const app = initializeApp(clientCredentials)
+const app = initializeApp(clientCredentials);
 
-export default app
-export const fireDB = getFirestore(app)
-export const fireAuth = getAuth()
-export const fireStorage = getStorage(app)
+export default app;
+export const fireDB = getFirestore(app);
+export const fireAuth = getAuth();
+export const fireStorage = getStorage(app);
